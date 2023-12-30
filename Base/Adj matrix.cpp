6d6c81d7b122cb6f47b418af6dcb8391dc8c48f1
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2023-12-30 06:03:25 +0100

    Update and rename Adj_matrix to Adj matrix.cpp

--- Base/Adj matrix.cpp
+++ Base/Adj matrix.cpp
@@ -7,15 +7,14 @@ int main()
     
     matrix[0][1]=1;
     matrix[1][0]=1;
-    
     matrix[1][2]=1;
     matrix[2][1]=1;
-        
+    
     matrix[1][3]=1;
     matrix[3][1]=1;
-    
     matrix[2][3]=1;
     matrix[3][2]=1;
+    
     for(int i=0;i<node;i++)
     {
         for(int j=0;j<node;j++)
