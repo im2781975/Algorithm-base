f9ddfad0863646b1e3c0bbb375bd101a9531f525
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-08 21:18:34 +0100

    Update Adj list.cpp

--- Base/Adj list.cpp
+++ Base/Adj list.cpp
@@ -1,22 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int node=4;
-    vector<int>adj_list[node];
-    
-    adj_list[0]={1};
-    adj_list[1]={0,2,3};
-    adj_list[2]={1,3};
-    adj_list[3]={1,2};
-    
-    for(int i=0;i<node;i++)
-    {
-        cout<<i<<"->";
-        for(int j=0;j<adj_list[i].size();j++)
-        {
-            cout<<adj_list[i][j]<<" ";
+
+void printPairs() {
+    int n = 4;
+    vector<vector<pair<int, int>>> adj_list(n);
+
+    adj_list[0] = {{1, 2}};
+    adj_list[1] = {{3, 4}};
+    adj_list[2] = {{1, 7}, {2, 3}};
+    adj_list[3] = {{9, 6}};
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < adj_list[i].size(); j++) {
+            cout << adj_list[i][j].first << " " << adj_list[i][j].second << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
 }
+
+int main() {
+    // printPairs();
+    vector<int> adj_list[4];
+
+    adj_list[0] = {1};
+    adj_list[1] = {0, 2, 3};
+    adj_list[2] = {1, 3};
+    adj_list[3] = {1, 2};
+
+    for (int i = 0; i < 4; i++) {
+        cout << i << "->";
+        for (int j = 0; j < adj_list[i].size(); j++) {
+            cout << adj_list[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+
