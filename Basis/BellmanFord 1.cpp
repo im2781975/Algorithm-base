1d97322da2cf69fca137ac4a060595bc197a2b8c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-08 17:20:15 +0100

    Update and rename Bellman_ford algo to BellmanFord 1.cpp

--- Basis/BellmanFord 1.cpp
+++ Basis/BellmanFord 1.cpp
@@ -17,8 +17,8 @@ int main()
     }
     for(int i=0; i<edge; i++)
     {
-        int u, v, w; // Added missing variable 'w' for edge weight.
-        cin >> u >> v >> w; // Read the edge weight 'w'.
+        int u, v, w;
+        cin >> u >> v >> w;
         adj_list[u].push_back({v, w});
     }
     int src = 1;
