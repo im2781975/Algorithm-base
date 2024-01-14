a9f812ab0755fdc59901d85235328902495d9036
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-14 10:16:10 +0100

    Update Adj List.cpp

--- Base/Adj List.cpp
+++ Base/Adj List.cpp
@@ -2,40 +2,38 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-// Function to convert an adjacency matrix to an adjacency list
-vector<vector<int>> adjacencyMatrixToList(vector<vector<int>>& adjacencyMatrix) {
-    int n = adjacencyMatrix.size();
-    vector<vector<int>> adjacencyList(n);
+vector<vector<int>> adjMatrixToList(vector<vector<int>>& adjMatrix) {
+    int n = adjMatrix.size();
+    vector<vector<int>> adjList(n);
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (adjacencyMatrix[i][j] == 1) {
-                adjacencyList[i].push_back(j);
+            if (adjMatrix[i][j] == 1) {
+                adjList[i].push_back(j);
             }
         }
     }
-    return adjacencyList;
+    return adjList;
 }
 int main() {
     int n;
     cout << "Enter the number of vertices: ";
     cin >> n;
-    vector<vector<int>> adjacencyMatrix(n, vector<int>(n));
+    vector<vector<int>> adjMatrix(n, vector<int>(n));
 
     cout << "Enter the adjacency matrix (0 or 1):\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cin >> adjacencyMatrix[i][j];
+            cin >> adjMatrix[i][j];
         }
     }
-    vector<vector<int>> adjacencyList = adjacencyMatrixToList(adjacencyMatrix);
+    vector<vector<int>> adjList = adjMatrixToList(adjMatrix);
 
     cout << "The adjacency list is:\n";
     for (int i = 0; i < n; ++i) {
         cout << "Vertex " << i << ": ";
-        for (int j = 0; j < adjacencyList[i].size(); ++j) {
-            cout << adjacencyList[i][j] << " ";
+        for (int j = 0; j < adjList[i].size(); ++j) {
+            cout << adjList[i][j] << " ";
         }
         cout << "\n";
     }
