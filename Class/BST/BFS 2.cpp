24c68f42ca76f7fdf0266b5306526e1224ca6f46
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-01-24 17:40:32 +0100

    Update and rename Node<build_tree> to BFS 2.cpp

--- Class/BST/BFS 2.cpp
+++ Class/BST/BFS 2.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 class node{
 public:
     int id;
@@ -8,17 +7,14 @@ public:
     node *Right;
     node *parent;
 };
-// Manually insert nodes in a binary tree and print information of the tree
 class BinaryTree{
 public:
     node* root;
     node* allnode[6];
-
     BinaryTree()
     {
         root = NULL;
     }
-
     node* CreateNewNode(int id)
     {
         node* newnode = new node;
@@ -28,38 +24,28 @@ public:
         newnode->parent = NULL;
         return newnode;
     }
-
-    void build_binary_tree()
+    void build()
     {
-
         for(int i=0;i<6;i++)
             allnode[i] = CreateNewNode(i);
 
         allnode[0]->Left = allnode[1];
         allnode[0]->Right = allnode[2];
-
         allnode[1]->Left = allnode[5];
         allnode[1]->parent = allnode[0];
-
         allnode[2]->Left = allnode[3];
         allnode[2]->Right = allnode[4];
         allnode[2]->parent = allnode[0];
-
         allnode[5]->parent = allnode[1];
-
         allnode[3]->parent = allnode[2];
         allnode[4]->parent = allnode[2];
         root = allnode[0];
     }
-
-    void print_tree_info()
+    void print()
     {
-
         for(int i=0;i<6;i++)
         {
-            int p = -1;
-            int l = -1;
-            int r = -1;
+            int p = -1, l = -1, r = -1;
             if(allnode[i]->parent!= NULL)
                 p = allnode[i]->parent->id;
             if(allnode[i]->Left!= NULL)
@@ -70,15 +56,11 @@ public:
             l<<" , Right child = "<<r<<"\n";
         }
     }
-
 };
-
-
 int main()
 {
     BinaryTree bt;
-    bt.build_binary_tree();
-    bt.print_tree_info();
+    bt.build();
+    bt.print();
     return 0;
 }
-
