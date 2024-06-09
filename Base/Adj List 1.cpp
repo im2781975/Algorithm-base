154eb5090547870fe912c9cf4e8dca57718a3006
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-09 15:21:16 +0200

    Update Adj List 1.cpp

--- Base/Adj List 1.cpp
+++ Base/Adj List 1.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    vector<int>v[5];
-    v[1].push_back(1);
-    v[1].push_back(2);
-    v[1].push_back(3);
-    for(int i=0; i<v[1].size(); i++)
-        cout << v[1][i] << " ";
+    vector <int> vec[5];
+    vec[1].push_back(2);
+    vec[1].push_back(3);
+    vec[1].push_back(4);
+    for(int i = 0; i < vec[1].size(); i++)
+        cout << vec[1][i] << " ";
 }
