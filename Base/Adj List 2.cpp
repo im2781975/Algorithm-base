41fe167a3d93359b721fd6f73259246ca6985f50
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-09 15:53:22 +0200

    Update Adj List 2.cpp

--- Base/Adj List 2.cpp
+++ Base/Adj List 2.cpp
@@ -1,22 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=1e9;
 int main()
 {
-    int n ,e;
-    cin >> n >> e;
-    vector<int>adjlist[n+1];
-    for(int i=1; i<=e; i++)
+    int node, edge;
+    cin >> node >> edge;
+    vector <int> adjlist[node + 1];
+    for(int i = 1; i <= edge; i++)
     {
         int u, v;
         cin >> u >> v;
         adjlist[u].push_back(v);
         adjlist[v].push_back(u);
     }
-    for(int i=1; i<=n; i++)
+    for(int i = 1; i <= node; i++)
     {
         cout << i << "->";
-        for(int j=0; j<=adjlist[i].size(); j++)
+        for(int j = 0; j< adjlist[i].size(); j++)
             cout << adjlist[i][j] << " ";
         cout << "\n";
     }
