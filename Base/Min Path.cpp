b6bc26e3451fadc96e8af55901f9092e9d6d1762
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-10 15:57:37 +0200

    Update Min Path.cpp

--- Base/Min Path.cpp
+++ Base/Min Path.cpp
@@ -1,31 +1,44 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-typedef long long ll;
-int solve(int n, int m, vector< vector< char > > &grid, vector< vector< ll > > dp)
-{
-    if (n==0 && m==0)
+
+using ll = long long;
+
+int solve(int n, int m, vector<vector<char>>& grid, vector<vector<ll>>& dp) {
+    if (n < 0 || m < 0 || grid[n][m] == '#') {
+        return 0;
+    }
+    if (n == 0 && m == 0) {
         return 1;
-    if (dp[n][m]!=-1)
+    }
+    if (dp[n][m] != -1) {
         return dp[n][m];
-    int ans=0;
-    if(n > 0 && grid[n][m]!='#')
-        ans = ans + solve(n, m-1, grid, dp);
-    if(m > 0 && grid[n][m]!='#')
-        ans = ans + solve(n-1, m, grid, dp);
+    }
+
+    int ans = 0;
+    if (n > 0) {
+        ans += solve(n - 1, m, grid, dp);
+    }
+    if (m > 0) {
+        ans += solve(n, m - 1, grid, dp);
+    }
+
     dp[n][m] = ans;
     return dp[n][m];
 }
-int main()
-{
+
+int main() {
     int n, m;
     cin >> n >> m;
-    vector<vector< char >>grid(n, vector<char>(m, 'a'));
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<m; j++)
+    vector<vector<char>> grid(n, vector<char>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
+        }
     }
-    vector<vector< ll > >dp(n + 5, vector<ll >(m+5, -1));
-    
-    cout << solve(n-1, m-1, grid, dp);
+    vector<vector<ll>> dp(n, vector<ll>(m, -1));
+
+    cout << solve(n - 1, m - 1, grid, dp) << endl;
+    return 0;
 }
+
