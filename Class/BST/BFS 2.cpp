4bc55f35981b0b77d22d68d5c2b5b09762fdcaa3
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-29 08:11:18 +0200

    Update BFS 2.cpp

--- Class/BST/BFS 2.cpp
+++ Class/BST/BFS 2.cpp
@@ -1,66 +1,59 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 class node{
-public:
+    public:
     int id;
-    node *Left;
-    node *Right;
-    node *parent;
+    node *left, *right, *parent;
 };
 class BinaryTree{
-public:
-    node* root;
-    node* allnode[6];
-    BinaryTree()
-    {
-        root = NULL;
+    public:
+    node *root, *allnode[6];
+    BinaryTree(){
+        root =NULL;
     }
-    node* CreateNewNode(int id)
-    {
-        node* newnode = new node;
-        newnode->id = id;
-        newnode->Left = NULL;
-        newnode->Right = NULL;
+    node *create(int id){
+        node *newnode = new node;
+        newnode-> id =id;
+        
+        newnode->left = NULL;
+        newnode->right = NULL;
         newnode->parent = NULL;
         return newnode;
     }
-    void build()
-    {
-        for(int i=0;i<6;i++)
-            allnode[i] = CreateNewNode(i);
-
-        allnode[0]->Left = allnode[1];
-        allnode[0]->Right = allnode[2];
-        allnode[1]->Left = allnode[5];
+    void build(){
+        for(int i = 0; i < 6; i++){
+            allnode[i] = create(i);
+        }
+        allnode[0]->left = allnode[1];
+        allnode[0]->right = allnode[2];
+        allnode[1]->left = allnode[5];
         allnode[1]->parent = allnode[0];
-        allnode[2]->Left = allnode[3];
-        allnode[2]->Right = allnode[4];
+        allnode[2]->left = allnode[3];
+        allnode[2]->right = allnode[4];
         allnode[2]->parent = allnode[0];
         allnode[5]->parent = allnode[1];
         allnode[3]->parent = allnode[2];
         allnode[4]->parent = allnode[2];
         root = allnode[0];
     }
-    void print()
-    {
-        for(int i=0;i<6;i++)
-        {
+    void print(){
+        for(int i = 0; i < 6; i++){
             int p = -1, l = -1, r = -1;
-            if(allnode[i]->parent!= NULL)
+            if(allnode[i]->parent!=NULL)
                 p = allnode[i]->parent->id;
-            if(allnode[i]->Left!= NULL)
-                l = allnode[i]->Left->id;
-            if(allnode[i]->Right!= NULL)
-                r = allnode[i]->Right->id;
-            cout<<"Node "<<i<<": Parent = "<<p<<" , Left child = "<<
-            l<<" , Right child = "<<r<<"\n";
+            if(allnode[i]->left!=NULL)
+                l = allnode[i]->left->id;
+            if(allnode[i]->right!=NULL)
+                r = allnode[i]->right->id;
+            cout << "Node: " << i;
+            cout << " parent: " << p;
+            cout << " Left: " << l;
+            cout << " Right: " << r << "\n";
         }
     }
 };
-int main()
-{
+int main(){
     BinaryTree bt;
     bt.build();
     bt.print();
-    return 0;
 }
