fae85c2b69f9b7366668bfb44c2777483654db88
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-06-29 22:14:49 +0200

    Update DFS 1.cpp

--- Class/BST/DFS 1.cpp
+++ Class/BST/DFS 1.cpp
@@ -1,8 +1,6 @@
-//DFS traversal from a given source vertex.traverses vertices reachable from s.
 #include<bits/stdc++.h>
 using namespace std;
-class Graph
-{
+class Graph{
     int V; 
     list<int> *adj; 
     public:
@@ -10,51 +8,42 @@ class Graph
     void addEdge(int v, int w);
     void DFS(int s);
 };
-Graph::Graph(int V)
-{
+Graph::Graph(int V){
     this->V = V;
     //dynamically allocate an array of adjacency lists for a graph with N vertices.
     adj = new list<int>[V];
 }
-void Graph::addEdge(int v, int w)
-{
-    adj[v].push_back(w);
+void Graph::addEdge(int u, int v){
+    adj[u].push_back(v);
 }
 // prints all not yet visited vertices reachable from s
-void Graph::DFS(int s)
-{
+void Graph::DFS(int src){
     vector<bool> visited(V, false);
-    stack<int> stack;
- 
-    stack.push(s);
-    while (!stack.empty())
-    {
-        int s = stack.top();
-        stack.pop();
- 
-        // Stack may contain same vertex twice. So we need to print the popped item only
-        // if it is not visited.
-        if (!visited[s])
-        {
-            cout << s << " ";
-            visited[s] = true;
+    stack<int> st;
+    st.push(src);
+    while (!st.empty()){
+        int src = st.top();
+        st.pop();
+        // Stack may contain same vertex twice. So we 
+        //need to print the popped item only if it is not visited.
+        if (!visited[src]){
+            cout << src << " ";
+            visited[src] = true;
         }
         // Get all adjacent vertices of the popped vertex s
         // If a adjacent has not been visited, then push it to the stack.
-        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
-            if (!visited[*i])
-                stack.push(*i);
+        for (auto it = adj[src].begin(); it!= adj[src].end(); ++it)
+            if (!visited[*it])
+                st.push(*it);
     }
 }
-int main()
-{
+int main(){
     Graph g(5);
     g.addEdge(1, 0);
     g.addEdge(0, 2);
     g.addEdge(2, 1);
     g.addEdge(0, 3);
     g.addEdge(1, 4);
- 
     cout << "Following is Depth First Traversal\n";
     g.DFS(0);
     return 0;
