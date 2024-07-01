50e601ed58ba5768fa912fad10ec91311e6c2b9c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-01 20:35:03 +0200

    Update BST 7.cpp

--- Class/BST/BST 7.cpp
+++ Class/BST/BST 7.cpp
@@ -1,20 +1,59 @@
-// printing vertical order of a given binary tree
-//hd of the root is taken as 0.For left child of any node, the hd =hd[node] -1.For the right child, hd = hd[node] +1
-//Vertical distance starts from the root at level 1 and increments by 1 for each level down the tree.
 #include <bits/stdc++.h>
-using namespace std;
 typedef long long int lli;
 typedef long long ll;
+using namespace std;
+// Structure for a binary tree node
 struct Node {
     int data;
     Node *left, *right;
 };
-struct Node* newNode(int data){
+struct Node* newNode(int data)
+{
     struct Node* node = new Node;
     node->data = data;
     node->left = node->right = NULL;
     return node;
 }
+// printing vertical order of a given binary tree using BFS
+void printVerticalOrder(Node* root){
+    if(root == NULL)
+        return;
+    // Create a map and store vertical order in map using function getVerticalOrder()
+    unordered_map<int, vector<int> > m;
+    int hd = 0;
+    // queue do level order traversal which contains node and horizontal distance
+    queue<pair<Node*, int> > q;
+    q.push({ root, hd });
+    // mn and mx contain the minimum and maximum horizontal distance from root
+    int mn = 0, mx = 0;
+    while (q.size() > 0) {
+        pair<Node*, int> temp = q.front();
+        q.pop();
+        hd = temp.second;
+        Node* node = temp.first;
+        m[hd].push_back(node->data);
+ 
+        if (node->left)
+            q.push({ node->left, hd - 1 });
+        if (node->right)
+            q.push({ node->right, hd + 1 });
+            
+        if (mn > hd)
+            mn = hd;
+        else if (mx < hd)
+            mx = hd;
+    }
+    // run the loop from minimum to maximum every horizontal distance hd
+    for (int i = mn; i <= mx; i++) {
+        vector<int> tmp = m[i];
+        for (int j = 0; j < tmp.size(); j++)
+            cout << tmp[j] << " ";
+        cout << endl;
+    }
+}
+// printing vertical order of a given binary tree
+//hd of the root is taken as 0.For left child of any node, the hd =hd[node] -1.For the right child, hd = hd[node] +1
+//Vertical distance starts from the root at level 1 and increments by 1 for each level down the tree.
 void preOrderTraversal(Node* root, lli hd, lli vd,
     map<lli, vector<int> >& m){
     if (!root)
@@ -36,7 +75,7 @@ void verticalOrder(Node* root)
     map<lli, vector<int> >::iterator it;
     for (it = mp.begin(); it != mp.end(); it++) {
         if (maxi != INT_MAX
-            && (it->first >> 30) != prekey) {
+            && (it->first >> 30) != maxi) {
             cout << endl;
         }
         maxi = it->first >> 30;
@@ -55,7 +94,9 @@ int main()
     root->right->right = newNode(7);
     root->right->left->right = newNode(8);
     root->right->right->right = newNode(9);
-    cout << "Vertical order traversal :- " << endl;
+    cout << "\nVertical order traversal is ";
+    printVerticalOrder(root);
+    cout << "\nVertical order traversal :- " << endl;
     verticalOrder(root);
     return 0;
 }
