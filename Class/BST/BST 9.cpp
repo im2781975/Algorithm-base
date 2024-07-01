6dff52183a5a5992885b6757d8ae57c1dea14886
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-01 20:25:23 +0200

    Update BST 9.cpp

--- Class/BST/BST 9.cpp
+++ Class/BST/BST 9.cpp
@@ -6,7 +6,6 @@ struct Node {
     int key;
     Node *left, *right;
 };
-// A function to create a new node
 Node* newNode(int key)
 {
     Node* node = new Node;
@@ -14,35 +13,29 @@ Node* newNode(int key)
     node->left = node->right = NULL;
     return node;
 }
-// The main function to print vertical order of a binary tree with given root
-void printVerticalOrder(Node* root)
-{
-    // Base case
-    if (!root)
+void printVerticalOrder(Node* root){
+    if(root == NULL)
         return;
     // Create a map and store vertical order in map using function getVerticalOrder()
     unordered_map<int, vector<int> > m;
     int hd = 0;
-    // Create queue to do level order traversal Every item of queue contains node and horizontal distance
+    // queue do level order traversal which contains node and horizontal distance
     queue<pair<Node*, int> > q;
     q.push({ root, hd });
- 
     // mn and mx contain the minimum and maximum horizontal distance from root
     int mn = 0, mx = 0;
     while (q.size() > 0) {
-        // pop from queue front
         pair<Node*, int> temp = q.front();
         q.pop();
         hd = temp.second;
         Node* node = temp.first;
-        // insert this node's data in vector of hash
         m[hd].push_back(node->key);
  
         if (node->left)
             q.push({ node->left, hd - 1 });
         if (node->right)
             q.push({ node->right, hd + 1 });
-        // Update mn and mx
+            
         if (mn > hd)
             mn = hd;
         else if (mx < hd)
