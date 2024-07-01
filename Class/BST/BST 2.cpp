669baf037910d5fd21339fb1985df43c5ba4e304
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-01 06:33:28 +0200

    Update BST 2.cpp

--- Class/BST/BST 2.cpp
+++ Class/BST/BST 2.cpp
@@ -46,6 +46,23 @@ class BT{
         return search(root, val);
     }
 };
+int findHeight(Tree *root){
+    if(root == nullptr)
+        return 0;
+    else{
+        int LHeight = findHeight(root->left);
+        int RHeight = findHeight(root->right);
+    return 1 + max(LHeight, RHeight);
+    }
+}
+int findMin(Tree *root){
+    if(root == nullptr)
+        return -1;
+    while(root->left!=nullptr){
+        root = root->left;
+    }
+    return root->data;
+}
 int main(){
     BT tree;
     tree.Insert(5);
@@ -57,6 +74,15 @@ int main(){
         cout << "Value 4 found in the tree." ;
     else
         cout << "Value 4 not found in the tree.";
+    //
+    Tree* root = new Tree(5);
+    root->left = new Tree(3);
+    root->right = new Tree(8);
+    root->left->left = new Tree(2);
+    root->left->right = new Tree(4);
+    int minVal = findMin(root);
+    cout << "The minimum value in the BST is: " << minVal << "\n";
+    int height = findHeight(root);
+    cout << "Height of the binary tree is: " << height ;
     return 0;
 }
-
