0f92552bfd768f08b8fc35328a4a651c6e9435ac
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-04 23:18:55 +0200

    Update and rename Queue<node> to Queue Node.cpp

--- Class/Queue Node.cpp
+++ Class/Queue Node.cpp
@@ -55,6 +55,49 @@ class Queue{
         return size;
     }
 };
+template<typename T>
+class node{
+    public:
+    T data;
+    node *nxt;
+    node(T val):data(val), nxt(nullptr){}
+};
+template<typename T>
+class Queue{
+    node <T> *front, *rear;
+    public:
+    Queue():front(nullptr), rear(nullptr){}
+    bool IsEmpty(){
+        return front == nullptr;
+    }
+    void enqueue(T val){
+        node <T> *newnode = new node<T>(val);
+        if(front == rear){
+            front = rear = newnode;
+            return;
+        }
+        rear->nxt = newnode;
+        rear = newnode;
+    }
+    void dequeue(){
+        if(IsEmpty()){
+            cout << "\nQueue is empty";
+            return;
+        }
+        node <T> *tmp = front;
+        front = front->nxt;
+        delete tmp;
+        if(front == nullptr)
+            rear = nullptr;
+    }
+    T peek(){
+        if(IsEmpty()){
+            cout << "Queue is Empty";
+            return T();
+        }
+        return front->data;
+    }
+};
 int main(){
     Queue q;
     q.enqueue(2);
@@ -65,5 +108,10 @@ int main(){
     q.dequeue();
     cout << "Front element: " << q.getfront() << "\n";
     cout << "Size is: " << q.getsize() << "\n";
-    
+    ////
+    Queue <int> q;
+    q.enqueue(2);
+    q.enqueue(4);
+    q.enqueue(7);
+    cout << "\nFront: " << q.peek();
 }
