3536f8cd2bfbf007d9f7802fce5a48acccc5de6b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-04 09:47:10 +0200

    Update CircularQ.cpp

--- Class/CircularQ.cpp
+++ Class/CircularQ.cpp
@@ -1,5 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
+template<typename T>
+class CircularQ{
+    T *arr;
+    int front, rear, cap;
+    public:
+    CircularQ(int sz){
+        cap = sz;
+        arr = new T[cap];
+        front = rear = -1;
+    }
+    bool IsEmpty(){
+        return front == -1;
+    }
+    // rear is one position behind front ((rear + 1) % capacity == front).
+    bool IsFull(){
+        return ((front == 0 && rear == cap - 1)||(rear == (front -1) % (cap - 1)));
+    }
+    void enqueue(T item){
+        if(IsFull()){
+            cout << "\nQueue is full";
+            return;
+        }
+        if(IsEmpty()){
+            front = rear = 0;
+            arr[rear] = item;
+        }
+        else{
+            rear = (rear + 1)% cap;
+            arr[rear] = item;
+        }
+    }
+    T dequeue(){
+        if(IsEmpty()){
+            cout << "\nQueue is empty";
+            return T();
+        }
+        T item = arr[front];
+        if(front == rear){
+            front = rear = -1;
+        }
+        else
+            front = (front + 1)%cap;
+        return item;
+    }
+    T getFront(){
+        if(IsEmpty()){
+            cout <<"\nQueue is empty";
+            return T();
+        }
+        return arr[front];
+    }
+};
 class CircularQ{
     int *arr, size, front, rear;
     public:
@@ -51,8 +103,10 @@ class CircularQ{
     }
 };
 int main(){
+    //For template base
+    CircularQ <int >q(5);
     CircularQ q(5);
-    cout << "Front Elements are: \n";
+    cout << "\nFront Elements are: ";
     for(int i = 1; i <= 5; i++){
         q.enqueue(i);
         cout << q.getFront() << " ";
@@ -61,5 +115,4 @@ int main(){
     cout << "\nFront element: " << q.getFront() << endl;
     q.enqueue(6);
     cout << "\nFront element after dequeue and enqueue: " << q.getFront() << endl;
-
 }
