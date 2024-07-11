9259f50ad92e4d454a9c6d9969e17cfff90f5a94
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-07-11 17:45:46 +0200

    Update and rename Stack.cpp to Stack arr.cpp

--- Class/Stack arr.cpp
+++ Class/Stack arr.cpp
@@ -1,4 +1,5 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<stack>
 using namespace std;
 class node{
     public:
@@ -51,6 +52,28 @@ class Stack{
         return tail->data;
     }
 };
+//Array Based Stack
+class Stack{
+    int arr[10], top;
+    public:
+    Stack():top(-1){}
+    void push(int val){
+        if(top < 9)
+            arr[++top] = val;
+        else{
+            cout << "\nStack is full";
+        }
+    }
+    int pop(){
+        if(top >= 0)
+            return arr[top--];
+        else
+            throw "Underflow";
+    }
+    bool IsEmpty(){
+        return top == -1;
+    }
+};
 int main(){
     Stack st;
     for(int i = 0; i < 6; i++){
@@ -60,4 +83,11 @@ int main(){
         cout << st.Top() << " ";
         st.Pop();
     }
+    //
+    Stack st;
+    for(int i = 1; i <= 5; i++){
+        st.push(i);
+        cout << st.pop() << " ";
+    }
+    cout << "\nIsEmpty: " << (st.IsEmpty()?"Yes":"No");
 }
