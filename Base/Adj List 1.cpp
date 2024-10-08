28eb7747b35da0285a00cfd7d1fb7012336878f9
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-08 17:38:31 +0200

    Update Adj List 1.cpp

--- Base/Adj List 1.cpp
+++ Base/Adj List 1.cpp
@@ -26,3 +26,19 @@ main(){
     }
 }
 /***/
+main(){
+    int node, edge; cin >> node >> edge;
+    vector <int> adjlist[node + 1];
+    for(int i = 0; i < edge; i++){
+        int u, v; cin >> u >> v;
+        adjlist[u].push_back(v);
+        adjlist[v].push_back(u);
+    }
+    for(int i = 0; i < node; i++){
+        cout << i << " > ";
+        for(int j = 0; j < adjlist[i].size(); j++)
+            cout << adjlist[i][j] << " ";
+        cout << "\n";
+    }
+}
+/***/
