808bcd64b7dda1c2eeca48e07eff7bcecae1869b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-08 18:12:05 +0200

    Update Adj Matrix 1.cpp

--- Base/Adj Matrix 1.cpp
+++ Base/Adj Matrix 1.cpp
@@ -1,26 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int maxn=1e9;
-int main()
-{
-    int n=9,m;
-    cin >> m;
-    int adjMatrix[n+1][n+1];
-    for(int i=0; i<=n; i++)
-    {
-        for(int j=0; j<=n; j++)
-            adjMatrix[i][j]=0;
+int main(){
+    int n; cin >> n;
+    int adjMatrix[n * n][n * n];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)
+            adjMatrix[i][j] = 0;
     }
-    for(int i=1; i<=m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adjMatrix[u][v]=1;
-        adjMatrix[v][u]=1;
+    for(int i = 1; i <= n; i++){
+        int u, v; cin >> u >> v;
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
     }
-    for(int i=0; i<=n; i++)
-    {
-        for(int j=0; j<=n; j++)
-           cout << adjMatrix[i][j] << " ";
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)
+            cout << adjMatrix[i][j] << " ";
+        cout << "\n";
     }
 }
