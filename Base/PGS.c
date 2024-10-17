e66d0743e068026b0d63123fc20c72a9d2af1ac4
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-17 11:21:40 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -9,3 +9,19 @@ int RicercaSequential(int *arr, int len, int val){
     return -1;
 }
 /***/
+int RicercaBinarySplit(int *arr, int val, int l, int r){
+    if(r < l)
+        return -1;
+    else{
+        int mid = (l + r) / 2;
+        if(arr[mid] == val)
+            return mid;
+        if(mid == 0)
+            return -1;
+        else if(val < arr[mid])
+            return RicercaBinarySplit(arr, val, l, mid - 1);
+        else
+            return RicercaBinarySplit(arr, val, mid + 1, r);
+    }
+}
+/***/
