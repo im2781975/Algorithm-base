c6977c6bccb3acde9dd319c946e7dfa3c43fdce7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-19 07:52:10 +0200

    Update Bipartite.cpp

--- Base/Bipartite.cpp
+++ Base/Bipartite.cpp
@@ -33,3 +33,42 @@ int main(){
     {0, 1, 0, 1}, {1, 0, 1, 0} };
     IsBaipartite(graph, 0)? cout << "Yes" : cout << "No";
 }
+/***/
+bool IsBipartite(int node, vector <int> adj[]){
+    //vector to store colour of vertex assigning all to -1
+    vector <int> color(node, -1);
+    queue <pair<int, int> >q;
+    for(int u = 0; u < node; u++){
+        if(color[u] == -1){
+            color[u] = 0;
+            q.push({u, 0});
+            while(!q.empty()){
+                pair <int, int> p = q.front();
+                q.pop();
+                //first node
+                int v = p.first;
+                //color of first node
+                int c = p.second;
+                for(int j : adj[v]){
+                    //if already coloured with parent vertex 
+                    //color then bipartite graph is not possible
+                    if(color[j] == c)
+                        return 0;
+                    if(color[j] == -1){
+                        color[j] = (c) ? 0 : 1;
+                        q.push({j, color[j]});
+                    }
+                        
+                }
+            }
+        }
+    }
+    return 1;
+}
+int main(){
+    int node = 4, edge = 8;
+    vector <int> adj[node];
+    adj[0] = {1, 3}; adj[1] = {0, 2};
+    adj[2] = {1, 3}; adj[2] = {0, 2};
+    (IsBipartite(node, adj))? cout << "Yes" : cout << "No";
+}
