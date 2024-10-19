743a16664ae5a20f52e0faba50fe19c03340ee7e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-19 11:46:55 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -220,3 +220,46 @@ while(swap):
             arr[i + 1] = tmp
             swap = True
 print(arr)
+/***/
+#include<iostream>
+using namespace std;
+void merge(int arr[], int l, int mid, int r){
+    int i = l, j = mid + 1, k = 0;
+    int tmp[r - l + 1];
+    for(int i = 0; i < (r - l + 1); i++)
+        tmp[i] = 0;
+    while(i <= mid && j <= r){
+        if(arr[i] <= arr[j]){
+            tmp[k] = arr[i]; i++;
+        }
+        else{
+            tmp[k] = arr[j]; j++;
+        }
+        k++;
+    }
+    while(i <= mid){
+        tmp[k] = arr[i];
+        i++; k++;
+    }
+    while(j <= r){
+        tmp[k] = arr[j];
+        j++; k++;
+    }
+    for(k = l; k <= r; k++)
+        arr[k] = tmp[k - l];
+}
+void mergesort(int arr[], int l, int r){
+    if(l < r){
+        int mid = (l + r)/2;
+        mergesort(arr, l, mid);
+        mergesort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+int main(){
+    int arr[8] = {41, 37, 10, 74, 98, 22, 83, 66}; 
+    int n = 8;
+    mergesort(arr, 0, n - 1);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
