3f7ee29e866b406a40ca37ef18171bf179fe5a41
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-20 15:40:44 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -356,3 +356,35 @@ int main(){
         cout << arr[i] << " ";
 }
 /***/
+#include <iostream> 
+using namespace std; 
+void heapify(int a[],int sx,int dx){
+    int temp=a[sx], i=sx, j= 2*i;
+    while (j <= dx){    
+        if((j < dx) && (a[j + 1] > a[j]))      
+            j++;    
+        if (temp < a[j]) {       a[i] = a[j];      
+            i = j;      
+            j = 2 * i;     
+        } 
+        else       
+            j = dx + 1;   
+    }      
+    if (i != sx)   
+    a[i] = temp;
+}  
+void heapsort(int a[], int n) {   int tmp,sx,dx;
+    for (sx = n / 2;sx >= 1;sx--)     heapify(a,sx,n); 
+    for (dx = n;dx > 1;dx--){     tmp = a[1];     
+        a[1] = a[dx];     
+        a[dx] = tmp;    
+        heapify(a,1,dx - 1);   
+    } 
+}
+int main(){
+    int a[]={0, 42, 38, 11, 75, 99, 23, 84, 67};   
+    int n=8;  
+    heapsort(a,n);   
+    for (int i=1;i<n+1;i++)     cout<<a[i]<<" "; 
+}  
+/***/
