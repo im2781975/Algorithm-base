d0aae9f3e6c469a1f44efdac5a68528d0aff6d5e
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-20 08:23:03 +0200

    Update Cycle detect.cpp

--- Base/Cycle detect.cpp
+++ Base/Cycle detect.cpp
@@ -38,3 +38,42 @@ int main(){
     }
     (CycleExits)?cout << "Cycle Exits" : cout << "Doesn't exits";
 }
+/***/
+using namespace std;
+bool IsCyclicConnected(vector <int> adj[], int v, vector <bool> &visited){
+    vector <int> parent(visited.size(), -1);
+    queue <int> q;
+    visited[v] = true;
+    q.push(v);
+    while(!q.empty()){
+        int u = q.front(); q.pop();
+        for(int child : adj[u]){
+            if(!visited[child]){
+                visited[child] = true;
+                q.push(child);
+                parent[child] = u;
+            }
+            else if(parent[u] != child)
+                return true;
+        }
+    }
+    return false;
+}
+bool IsCyclicDisconnected(vector <int> adj[], int node){
+    vector <bool> visited(node, false);
+    for(int v = 0; v < node; v++){
+        if(!visited[v] && IsCyclicConnected(adj, v, visited))
+            return true;
+    }
+    return false;
+}
+int main(){
+    int node, edge; cin >> node >> edge;
+    vector <int> adj[node];
+    for(int i = 0; i < edge; i++){
+        int u, v; cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    (IsCyclicDisconnected(adj, node)) ? cout << "Yes" : cout << "No";
+}
