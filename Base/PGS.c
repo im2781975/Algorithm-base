5eee4745691d8c2d365e60215a371cd069802496
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-20 16:47:59 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -388,3 +388,29 @@ int main(){
     for (int i=1;i<n+1;i++)     cout<<a[i]<<" "; 
 }  
 /***/
+def heapify(a,sx,dx):      
+    temp=a[sx]   
+    i=sx   
+    j= 2*i      
+    while j <= dx:     
+        if j < dx and a[j + 1] > a[j]:       
+            j+=1     
+        if temp < a[j]:       
+            a[i] = a[j]       
+            i = j       
+            j = 2 * i     
+        else:       
+            j = dx + 1      
+    if i != sx:     
+        a[i] = temp    
+def heapsort(a,n): 
+    for sx in range(n//2,0,-1):     heapify(a,sx,n)     
+    for dx in range(n,1,-1):     tmp = a[1]     
+        a[1] = a[dx]     
+        a[dx] = tmp     
+        heapify(a,1,dx-1)   
+a=[0, 42, 38, 11, 75, 99, 23, 84, 67]
+n=8 
+heapsort(a,n)
+print(a) 
+/***/
