3a91f1a67393c74d67bbe3b0faae0c8a615a30d5
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-22 05:54:16 +0200

    Update Dijkstra 1.cpp

--- Base/Dijkstra 1.cpp
+++ Base/Dijkstra 1.cpp
@@ -1,56 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <climits>
+#include<iostream>
+#include<vector>
+#include<climits>
 using namespace std;
-const int INF = INT_MAX;
-
-int minDistance(vector<int>& dist, vector<bool>& sp, int n) {
-    int minDist = INF, minIndex;
-
-    for (int i = 0; i < n; i++) {
-        if (!sp[i] && dist[i] <= minDist) {
-            minDist = dist[i];
-            minIndex = i;
+int minDist(vector <int> dist, vector <bool> visited, int node){
+    int mini = INT_MAX, minidx;
+    for(int i = 0; i < node; i++){
+        if(!visited[i] && dist[i] <= mini){
+            mini = dist[i];
+            minidx = i;
         }
     }
-    return minIndex;
+    return minidx;
 }
-void dijkstra(vector<vector<int>>& graph, int src, int n) {
-    vector<int> dist(n, INF);
-    vector<bool> sp(n, false);
-
+void Dijkstra(vector <vector <int> > &graph, int src, int node){
+    vector <int> dist(node, INT_MAX);
+    vector <bool> visited(node, false);
     dist[src] = 0;
-    for (int i = 0; i < n - 1; i++) {
-        int u = minDistance(dist, sp, n);
-        sp[u] = true;
-
-        for (int v = 0; v < n; v++) {
-            if (!sp[v] && graph[u][v] && dist[u] != INF && (dist[u] + graph[u][v] < dist[v])) {
+    for(int i = 0; i < node - 1; i++){
+        int u = minDist(dist, visited, node);
+        visited[u] = true;
+        for(int v = 0; v < node; v++){
+            if(!visited[v] && graph[u][v] && dist[u] != INT_MAX && (dist[u] + graph[u][v]) < dist[v])
                 dist[v] = dist[u] + graph[u][v];
-            }
         }
     }
-    cout << "Vertex \t Distance from Source" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << i << " \t " << dist[i] << endl;
-    }
+    for(int i = 0; i < node; i++)
+        cout << i << "\t" << dist[i] << "\n";
 }
-int main() {
-    int n;
-    cout << "Enter the number of vertices: ";
-    cin >> n;
-
-    vector<vector<int>> graph(n, vector<int>(n));
-    cout << "Enter the adjacency matrix:" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+int main(){
+    int node; cin >> node;
+    vector <vector <int> >graph(node, vector <int>(node));
+    for(int i = 0; i < node; i++){
+        for(int j = 0; j < node; j++)
             cin >> graph[i][j];
-        }
     }
-    int src;
-    cout << "Enter the source vertex: ";
-    cin >> src;
-    dijkstra(graph, src, n);
-    return 0;
+    int src; cin >> src;
+    Dijkstra(graph, src, node);
 }
-
