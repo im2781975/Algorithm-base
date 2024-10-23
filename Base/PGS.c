f45797099b731228697c8d2a123f9495e9360222
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-23 10:31:17 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -1,4 +1,39 @@
 /***/
+#include <stdio.h> 
+#include <stdlib.h>  
+#define MAX_SIZE 100  
+int queue[MAX_SIZE]; 
+int front = 0; 
+int rear = -1; 
+int item_count = 0;  
+void insert(int data){     
+    if (item_count == MAX_SIZE){         printf("Error: Queue overflow\n");     
+    } else{         
+        rear = (rear + 1) % MAX_SIZE;        
+        queue[rear] = data;         item_count++;    
+    }
+}  
+int remove_data(){     
+    int data;     
+    if (item_count == 0){         printf("Error: Queue underflow\n");         return -1;     
+    } else {         
+        data = queue[front];         front = (front + 1) % MAX_SIZE;         
+        item_count--;         
+        return data;     
+    } 
+}  
+void print_queue(){     
+    int i;     
+    if (item_count == 0){         
+        printf("Error: Queue is empty\n");     
+    } else {         
+        for (i = 0; i < item_count; i++){             
+            int index = (front + i) % MAX_SIZE; 
+            printf("%d\n", queue[index]);   
+        }     
+    } 
+} 
+/***/
 #include <iostream> 
 using namespace std;  
 class Stack{ 
