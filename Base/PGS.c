88588457d973a064a53fd557c9e82f2b44bcafbf
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-23 11:19:48 +0200

    Update PGS.c

--- Base/PGS.c
+++ Base/PGS.c
@@ -1,4 +1,41 @@
 /***/
+class CircularQueue:     
+    def __init__(self, size):       self.size = size        
+        self.queue = [None] * size
+        self.head = self.tail = -1
+    def enqueue(self, item):         if ((self.tail + 1) % self.size == self.head):             print("Queue is Full")  elif (self.head == -1):
+            self.head = 0       
+            self.tail = 0   
+            self.queue[self.tail] = item         
+        else:             
+            self.tail = (self.tail + 1) % self.size
+            self.queue[self.tail] = item          
+    def dequeue(self):        
+        if (self.head == -1):       print("Queue is Empty")         
+        elif (self.head == self.tail):             temp = self.queue[self.head]             
+            self.head = -1  
+            self.tail = -1  
+            return temp 
+        else:             
+            temp = self.queue[self.head]        
+            self.head = (self.head + 1) % self.size
+            return temp    
+    def display(self):        
+        if(self.head == -1):        print("No element in the Queue")         
+        elif (self.tail >= self.head):             
+            for i in range(self.head, self.tail + 1):                 print(self.queue[i], end=" ")         
+        else:             
+            for i in range(self.head, self.size):  print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):       print(self.queue[i], end=" ")  
+queue = CircularQueue(5) 
+queue.enqueue(1) 
+queue.enqueue(2)
+queue.enqueue(3)
+queue.display() 
+print("\n") 
+print(queue.dequeue())
+queue.display() 
+/***/
 class Queue: 
     def __init__(self): 
         self.queue = [] 
