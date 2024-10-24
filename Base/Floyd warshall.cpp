edc1cbc1dc3fea11825bfbdab173cd7b7f074b38
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-24 08:25:30 +0200

    Update Floyd warshall.cpp

--- Base/Floyd warshall.cpp
+++ Base/Floyd warshall.cpp
@@ -1,46 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=1e3;
-const int INF=1e9;
-int d[N][N];
-
-int main()
-{
-    int node,edge;
-    cin>>node>>edge;
-    for(int i=1; i<=node; i++)
-    {
-        for(int j=1; j<=node; j++)
-        {
-            d[i][j]=INF;
+const int INF = 1e9, N = 505;
+int dist[N][N];
+int main(){
+    int node, edge; cin >> node >> edge;
+    for(int i = 1; i <= node; i++){
+        for(int j = 1; j <= node; j++)
+        if(i == j)
+            dist[i][j] = 0;
+        else
+            dist[i][j] = INT_MAX;
+    }
+    for(int i = 0; i < edge; i++){
+        int u, v, w; cin >> u >> v >> w;
+        dist[u][v] = w;
+    }
+    for(int k = 1; k <= node; k++){
+        for(int u = 1; u <= node; u++){
+            for(int v = 1; v <= node; v++)
+            if(dist[u][k] < INF && dist[k][v] < INF)
+                dist[u][v] = min(dist[u][v], dist[u][k] + dist[k][v]);
         }
     }
-    for(int i=0; i<edge; i++)
-    {
-        int u,v,w;
-        cin>>u>>v>>w;
-        d[u][v]=w;
-    }
-    for(int i=1; i<=node; i++)
-    {
-        d[i][i]=0;
-    }
-    for(int k=1; k<=node; k++)
-    {
-        for(int u=1; u<=node; u++)
-        {
-            for(int v=1;v<=node; v++)
-            {
-                d[u][v]=min(d[u][v],d[u][k]+d[k][v]);
-            }
-        }
-    }
-    for(int i=1; i<=node; i++)
-    {
-        for(int j=1; j<=node; j++)
-        {
-            cout<<d[i][j]<<" ";
-        }
-        cout<<"\n";
+    for(int i = 1; i < node; i++){
+        for(int j = 1; j <= node; j++)
+        if(dist[i][j] == INF)
+            cout << "__";
+        else
+            cout << dist[i][j] << " ";
+        cout << "\n";
     }
 }
