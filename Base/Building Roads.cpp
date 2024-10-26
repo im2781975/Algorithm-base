bda465bb2222b4bbea4a55b5140d38e9034a1434
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-26 07:57:32 +0200

    Update Building Roads.cpp

--- Base/Building Roads.cpp
+++ Base/Building Roads.cpp
@@ -58,3 +58,47 @@ int main(){
             cout << "Not connected";
     }
 }
+using namespace std;
+//find out whether a given graph can be converted to two Cliques or not
+const int node = 5;
+bool IsBipartite(int graph[][node], int src, int color[]){
+    color[src] = 1;
+    queue <int> q; q.push(src);
+    while(!q.empty()){
+        int u = q.front(); q.pop();
+        for(int v = 0; v < node; v++){
+            if(graph[u][v] && color[v] == -1){
+                color[v] = 1 - color[u];
+                q.push(v);
+            }
+            else if(graph[u][v] && color[u] == color[v])
+                return false;
+        }
+    }
+    return true;
+}
+bool IsBicolorable(int graph[][node]){
+    int color[node];
+    for(int i = 0; i < node; i++)
+        color[i] = -1;
+    for(int i = 0; i < node; i++){
+        if(color[i] == -1){
+            if(!IsBipartite(graph, i, color))
+                return false;
+        }
+    }
+    return true;
+}
+int main(){
+    int g[node][node] = 
+    {{0, 1, 1, 1, 0},{1, 0, 1, 0, 0},{1, 1, 0, 0, 0},
+    {0, 1, 0, 0, 1},{0, 0, 0, 1, 0}};
+    int graph[node][node];
+    for(int i = 0; i < node; i++){
+        for(int j = 0; j < node; j++){
+            //Ignore diagonal.
+            graph[i][j] = (i != j) ? !g[i][j] : 0;
+        }
+    }
+    (IsBicolorable(graph)) ? cout << "Yes" : cout << "No";
+}
