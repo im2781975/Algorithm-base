f4230918d3f655ec8eceb64b2b3fe80310a05f7c
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-27 05:59:50 +0100

    Update Count Room.cpp

--- Base/Count Room.cpp
+++ Base/Count Room.cpp
@@ -67,3 +67,34 @@ int main(){
     }
     cout << roomCnt << " " << LongestRoom << '\n';
 }
+//count the number of connected components in an undirected graph.
+using namespace std;
+void addEdge(vector <int> adj[], int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+void DFS(vector <int> adj[], int u, vector <bool> &visited){
+    visited[u] = true;
+    for(int v = 0; v < adj[u].size(); v++){
+        if(!visited[adj[u][v]]){
+            DFS(adj, adj[u][v], visited);
+        }
+    }
+}
+int main(){
+    int node = 5;
+    vector <int> adj[node];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 2);
+    addEdge(adj, 0, 3);
+    addEdge(adj, 3, 4);
+    vector <bool> visited(node, false);
+    int cnt = 0;
+    for(int i = 0; i < node; i++){
+        if(!visited[i]){
+            DFS(adj, i, visited);
+            cnt++;
+        }
+    }
+    cout << cnt;
+}
