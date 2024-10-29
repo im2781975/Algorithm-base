616f13d6b51d6092fb1f378b7666abae78933920
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-29 09:04:43 +0100

    Update Generate graph.cpp

--- Base/Generate graph.cpp
+++ Base/Generate graph.cpp
@@ -1,6 +1,15 @@
-#include<bits/stdc++.h>
 //generate a graph for a given fixed degrees
 using namespace std;
+// Function to validate if the degree sequence can represent a simple undirected graph.
+bool Isvalid(const int Deg[], int n){
+    int DegreeSum = 0;
+    for(int i = 0; i < n; i++){
+        if(Def[i] < 0 || Deg[i] >= n)
+            return false;
+        DegreeSum += Deg[i]
+    }
+    return DegreeSum % 2 == 0;
+}
 void print(int DegSeq[], int n){
     int mat[n][n] = {0};
     for(int i = 0; i < n; i++){
@@ -27,5 +36,8 @@ void print(int DegSeq[], int n){
 int main(){
     int DegSeq[]{2, 2, 1, 1, 1};
     int n = sizeof(DegSeq)/sizeof(DegSeq[0]);
-    print(DegSeq, n);
+    if(Isvalid(DegSeq, n))
+        print(DegSeq, n);
+    else
+        cout << "The degree sequence is invalid for generating a simple undirected graph.\n".
 }
