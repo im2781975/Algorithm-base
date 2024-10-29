93f455fb98b5f278ff14d5173ffb428f01b101f7
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-10-29 06:01:44 +0100

    Update Edge list.cpp

--- Base/Edge list.cpp
+++ Base/Edge list.cpp
@@ -1,33 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-void First()
-{
-vector<vector<int>> edge_list(4); 
-    edge_list[0].push_back(0);
-    edge_list[0].push_back(1);
-    edge_list[1].push_back(1);
-    edge_list[1].push_back(2);
-
-    edge_list[2].push_back(1);
-    edge_list[2].push_back(3);
-    edge_list[3].push_back(2);
-    edge_list[3].push_back(3);
-    
-    
-    for (int i = 0; i < edge_list.size(); i++) {
-        for (int j = 0; j < edge_list[i].size(); j++) {
-            cout << edge_list[i][j] << " ";
-        }
+int main(){
+    vector <vector <int>> edgeList(4);
+    edgeList[0].push_back(0);
+    edgeList[0].push_back(1);
+    edgeList[1].push_back(2);
+    edgeList[1].push_back(1);
+    edgeList[2].push_back(1);
+    edgeList[2].push_back(3);
+    edgeList[3].push_back(2);
+    edgeList[3].push_back(3);
+    for(int i = 0; i < edgeList.size(); i++){
+        for(int j = 0; j < edgeList[i].size(); j++)
+            cout << edgeList[i][j] << " ";
         cout << "\n";
     }
 }
-int main() {
-    //First();
-    vector <vector<int > >edge_list{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12},};
-    edge_list.push_back({13, 14, 15});
-    for(int i=0; i<edge_list.size(); i++)
-    {
-        cout<<edge_list[i][0] << " "<<edge_list[i][1] <<" " <<edge_list[i][2] <<"\n";
+using namespace std;
+int main(){
+    vector <vector <int> >edgeList 
+    {{1, 2, 3}, {4, 5, 6}, 
+    {7, 8, 9}, {10, 11, 12},};
+    edgeList.push_back({13, 14, 15});
+    for(int i = 0; i < edgeList.size(); i++){
+        cout << edgeList[i][0] << " " << edgeList[i][1] << " " << edgeList[i][2] << " ";
+        cout << "\n";
     }
 }
-
