d10d6c4b940abdfaf52597e7aa7d1bcab2d0d315
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2024-11-01 07:32:30 +0100

    Update Sortest Dist.cpp

--- Base/Sortest Dist.cpp
+++ Base/Sortest Dist.cpp
@@ -1,35 +1,21 @@
-// find shortest distance in a multistage graph.
+//find shortest distance in a multistage graph.
 #include<bits/stdc++.h>
 using namespace std;
-#define N 8
 #define INF INT_MAX
-// Returns shortest distance from 0 to N-1.
-int shortestDist(int graph[N][N]) {
- 
-    // dist[i] is going to store shortest distance from node i to node N-1.
-    int dist[N];
-    dist[N-1] = 0;
-    // Calculating shortest path for rest of the nodes
-    for (int i = N-2 ; i >= 0 ; i--)
-    {
-        // Initialize distance from i to destination (N-1)
+#define N 8
+int shortestDist(int graph[N][N]){
+    int dist[N]; dist[N - 1] = 0;
+    for(int i = N - 2; i >= 0; i--){
         dist[i] = INF;
-        // Check all nodes of next stages to find shortest distance from i to N-1.
-        for (int j = i ; j < N ; j++)
-        {
-            // Reject if no edge exists
-            if (graph[i][j] == INF)
+        for(int j = i; j < N; j++){
+            if(graph[i][j] == INF)
                 continue;
- 
-            // We apply equation to distance to target through j. and compare with minimum distance  so far.
             dist[i] = min(dist[i], graph[i][j] + dist[j]);
         }
     }
     return dist[0];
 }
-int main()
-{
-    // Graph stored in the form of an adjacency Matrix
+int main(){
     int graph[N][N] =
       {{INF, 1, 2, 5, INF, INF, INF, INF},
        {INF, INF, INF, INF, 4, 11, INF, INF},
@@ -40,5 +26,4 @@ int main()
        {INF, INF, INF, INF, INF, INF, INF, 2},
       {INF, INF, INF, INF, INF, INF, INF, INF}};
     cout << shortestDist(graph);
-    return 0;
 }
