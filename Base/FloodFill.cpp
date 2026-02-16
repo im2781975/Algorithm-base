ee8b99512cc89fcf5e02b7b8f9f93ad8cdc392fb
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2026-02-16 10:56:28 +0600

    Update FloodFill.cpp

--- Base/FloodFill.cpp
+++ Base/FloodFill.cpp
@@ -25,3 +25,61 @@ vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int ne
         dfs(sr, sc, image, newColor, oldColor, n, m);
     return image;
 }
+// Function that returns true if the given pixel is valid
+bool isValid(int screen[][8], int m, int n, int x, int y,
+             int prevC, int newC)
+{
+    if (x < 0 || x >= m || y < 0 || y >= n
+        || screen[x][y] != prevC || screen[x][y] == newC)
+        return false;
+    return true;
+}
+void floodFill(int screen[][8], int x, int y, int prevC, int newC) {
+   /* int m = screen.size(), n = screen[0].size();
+    int x = 4, y = 4;
+    int prevC = screen[x][y];
+    // New color that has to be filled
+    int newC = 3;
+    floodFill(screen, m, n, x, y, prevC, newC); */
+    queue<pair<int, int> > queue;
+    pair<int, int> p(x, y);
+    queue.push(p);
+    // Color the pixel with the new color
+    screen[x][y] = newC;
+    while (queue.size() > 0) {
+        pair<int, int> currPixel = queue.front();
+        queue.pop();
+ 
+        int posX = currPixel.first;
+        int posY = currPixel.second;
+        if (isValid(screen, m, n, posX + 1, posY, prevC,
+                    newC)) {
+            screen[posX + 1][posY] = newC;
+            p.first = posX + 1;
+            p.second = posY;
+            queue.push(p);
+        }
+        if (isValid(screen, m, n, posX - 1, posY, prevC,
+                    newC)) {
+            screen[posX - 1][posY] = newC;
+            p.first = posX - 1;
+            p.second = posY;
+            queue.push(p);
+        }
+        if (isValid(screen, m, n, posX, posY + 1, prevC,
+                    newC)) {
+            screen[posX][posY + 1] = newC;
+            p.first = posX;
+            p.second = posY + 1;
+            queue.push(p);
+        }
+ 
+        if (isValid(screen, m, n, posX, posY - 1, prevC,
+                    newC)) {
+            screen[posX][posY - 1] = newC;
+            p.first = posX;
+            p.second = posY - 1;
+            queue.push(p);
+        }
+    }
+}
